89e77b76f6378f1ae06edab85f6264b09da01e32
Niru Bhandari <nirubhandari45@gmail.com>
2020-11-22 21:34:46 +0545

    Dropdown using javascript

---
+++ dropdownjs/script.js
@@ -0,0 +1,20 @@
+function show_hide_services(){
+    var click= document.getElementById("drop_content_services");
+    if (click.style.display==="none"){
+        click.style.display= "block";
+    }
+    else{
+        click.style.display="none";
+    }
+}
+
+
+function show_hide_language(){
+    var click= document.getElementById("drop_content_language");
+    if (click.style.display==="none"){
+        click.style.display= "block";
+    }
+    else{
+        click.style.display="none";
+    }
+}
